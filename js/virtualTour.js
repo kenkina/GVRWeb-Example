b99a74a2db4011da492ce2a75c5c996ff1b1469a
kenkina <ken.kina97@gmail.com>
2017-03-26 16:48:09 -0500

    15 commit spiral

--- js/virtualTour.js
+++ js/virtualTour.js
@@ -29,7 +29,7 @@ var scenes = {
 
 function onLoad() {
   vrView = new VRView.Player('#vrview', {
-    image: 'img/LoadingFire.gif',
+    image: 'img/LoadingSpiral.gif',
     is_stereo: false,
     width: '100%',
     height: 350
